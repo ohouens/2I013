6168db3d10b08e22464ce1e0962f1bc018108b52
Caroline HUYNH <3520765@ppti-14-406-09.ufr-info-p6.jussieu.fr>
2018-04-05 11:47:54 +0200

    reglage du compteur et modification de la strategie

--- controler.py
+++ controler.py
@@ -13,8 +13,8 @@ class TestControler(object):
         self.stop = False
         self.lastPosition = self.robot.get_position()
         self.currentPosition = self.robot.get_position()
-        self.strategie = 3
-        self.etat = 1
+        self.strategie = 0
+        self.etat = 2
         self.tour = 0
         self.distance = 0
         #self.vue = Vue2D(self)
@@ -27,6 +27,8 @@ class TestControler(object):
         self.currentPosition = self.robot.get_position()
         if(self.etat == 0):
             if(self.strategie == 0):
+                self.robot.stop()
+                self.stop()
                 exit(0)
             elif(self.strategie == 1):
                 self.tour = 0
@@ -46,6 +48,7 @@ class TestControler(object):
             self.robot.set_led(self.robot.LED_RIGHT_EYE, 255, 0, 255)
             self.robot.forward(300)
             self.distance += self.robot.distance(self.lastPosition, self.currentPosition)
+            #self.etat = 4
             print("{0} > {1}\n".format(self.distance, 700-(2*math.pi*(self.robot.WHEEL_DIAMETER/2))))
             if(self.distance > 700-(2*math.pi*(self.robot.WHEEL_DIAMETER/2))):
                 self.robot.set_led(self.robot.LED_RIGHT_EYE, 255, 255, 255)
@@ -63,22 +66,22 @@ class TestControler(object):
             self.robot.set_led(self.robot.LED_RIGHT_EYE, 255, 255, 0)
             self.robot.rotate(300)
             self.distance += self.robot.distance(self.lastPosition, self.currentPosition)
-            print("{0} > {1}\n".format(self.distance, math.pi*(self.robot.WHEEL_DIAMETER/1.4)/4))
-            if(self.distance>math.pi*(self.robot.WHEEL_DIAMETER/1.4)/4):
+            print("{0} > {1}\n".format(self.distance, math.pi*(self.robot.WHEEL_DIAMETER)/4))
+            if(self.distance>math.pi*(self.robot.WHEEL_DIAMETER)/4):
                 self.robot.stop()
                 self.robot.set_led(self.robot.LED_LEFT_EYE, 0, 255, 0)
                 self.distance = 0
                 self.tour += 1
-                self.etat = 1
+                self.etat = 0
                 #self.stop = True
         elif(self.etat == 3):
             self.robot.forward(50)
             self.robot.rotate(50)
-        elif(self.etat == 5):
-            if(cpt%250):
+        elif(self.etat == 4):
+            if(self.cpt%250):
                 self.robot.save_image(self.cpt)
-                self.ctp+=1
-                self.etat=0
+            self.cpt+=1
+            self.etat=1
         elif(self.etat == 45):
             self.robot.set_led(self.robot.LED_LEFT_EYE, 0, 255, 255)
             self.robot.set_led(self.robot.LED_RIGHT_EYE, 255, 0, 255)
