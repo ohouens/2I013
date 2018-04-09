be96aa8a11191a9f189ffc1f68113edb26915eaa
OHOUENS <3670015@ppti-14-308-13.ufr-info-p6.jussieu.fr>
2018-04-09 13:53:48 +0200

    ajout d'exemples pour le déplacement

--- test2.py
+++ test2.py
@@ -2,10 +2,11 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 import sys
+import time
 
 display=(400,400)
 
-vertices=(
+verticies=(
 	(1, -1, -1),
 	(1, 1, -1),
 	(-1, 1, -1),
@@ -31,12 +32,66 @@ edges=(
 	(5,7)
 	)
 
+colors = (
+    (1,0,0),
+    (0,1,0),
+    (0,0,1),
+    (0,1,0),
+    (1,1,1),
+    (0,1,1),
+    (1,0,0),
+    (0,1,0),
+    (0,0,1),
+    (1,0,0),
+    (1,1,1),
+    (0,1,1),
+    )
+
+surfaces = (
+    (0,1,2,3),
+    (3,2,7,6),
+    (6,7,5,4),
+    (4,5,1,0),
+    (1,5,7,2),
+    (4,0,3,6)
+    )
+
+ground_surfaces = (0,1,2,3)
+
+ground_vertices = (
+    (-10,-0.1,50),
+    (10,-0.1,50),
+    (-10,-0.1,-300),
+    (10,-0.1,-300),
+    )
+
+def Ground():
+    
+    glBegin(GL_QUADS)
+
+    x = 0
+    for vertex in ground_vertices:
+        x+=1
+        glColor3fv((0,1,1))
+        glVertex3fv(vertex)
+        
+    glEnd()
+
 def Cube():
-	glBegin(GL_LINES)
-	for edge in edges:
-		for vertex in edge:
-			glVertex3fv(vertices[vertex])
-	glEnd()
+    glBegin(GL_QUADS)
+    for surface in surfaces:
+        x = 0
+        for vertex in surface:
+            x+=1
+            glColor3fv(colors[x])
+            glVertex3fv(verticies[vertex])
+    glEnd()
+
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(verticies[vertex])
+    glEnd()
 
 def refresh3d(width, height):
     glViewport(0, 0, display[0], display[1])
@@ -47,13 +102,14 @@ def refresh3d(width, height):
     glLoadIdentity()
 
 def draw():
-	#glRotatef(1,3,1,1)
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 	#glLoadIdentity()
+	glRotatef(1,0,1,0)
+	glTranslatef(0,0,-1)
 	#refresh3d(width, height)
-
-	glColor3f(1.0, 0.0, 1.0)
+	#Ground()
 	Cube()
+	time.sleep(1./1)
 
 	glutSwapBuffers()
 
