9c344629561c86671187bf4f471a59003ccfc39b
carowin <carolinehuynh.h@gmail.com>
2018-05-04 13:13:10 +0200

    Amelioration 3D

--- simulation/basiques/balise.py
+++ simulation/basiques/balise.py
@@ -11,7 +11,7 @@ class Balise(Cube):
 
     def __init__(self, x, y, z):
         """Constructeur de la classe Mur"""
-        Cube.__init__(self,x,y,z,100,100,100)
+        Cube.__init__(self,x,y,z,300,10,200)
 
     def safficher(self):
         """Methode d'affichage d'un mur au format :
